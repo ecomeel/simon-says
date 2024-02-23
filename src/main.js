1fafc15e72f7867293188ca7f331709ecd304e39
Aleksandr Pulin <ecomeel@gmail.com>
2024-02-23 14:22:55 +0300

    config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import vueConfig from '../vue.config';
 import './style.css'
 import App from './App.vue'
 
+vueConfig.devtools = true;
 // createApp(App).mount('#app');
 const app = createApp(App);
 app.mount('#app');
